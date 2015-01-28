f7cffe89f448f723c97bf5db82aa82f87f1f62a8
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 14:20:41 -0500

    Ajax to add game to group page upon creation

--- public/js/application.js
+++ public/js/application.js
@@ -17,4 +17,19 @@ $(document).ready(function() {
       $('#group_games_ct').append(response);
     })
   })
+
+  var $create_game_form = $('#create_game_form');
+
+  $create_game_form.on('submit', function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+
+    $.ajax({
+      type: 'PUT',
+      url: $target.attr('action'),
+      data: $target.serialize()
+    }).done(function(response) {
+      $('#main_container').html(response);
+    })
+  })
 });
