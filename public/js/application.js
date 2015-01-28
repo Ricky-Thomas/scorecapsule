029bbe5027f21324291500d05644741f81b2c932
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 18:23:09 -0500

    Edits ajax call

--- public/js/application.js
+++ public/js/application.js
@@ -15,23 +15,21 @@ $(document).ready(function() {
       data: $target.serialize()
     }).done(function(response) {
       $('#group_games_ct').append(response);
-    })
-  })
+    });
+  });
 
-  var $create_game_form = $('#create_game_form');
-
-  $create_game_form.on('submit', function(event) {
+  $('#group_games_ct').on('submit', "#create_game", function(event) {
     event.preventDefault();
     var $target = $(event.target);
 
     $.ajax({
-      type: 'PUT',
+      type: 'POST',
       url: $target.attr('action'),
       data: $target.serialize()
     }).done(function(response) {
       $('#main_container').html(response);
-    })
-  })
+    });
+  });
 
   var $add_clash_button = $('.add_clash_button');
 
