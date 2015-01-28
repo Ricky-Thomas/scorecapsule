fb244461345fdf61eab6805b00d20d4cff6a71c9
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 15:30:19 -0500

    Makes it so Usernames must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :user_groups
   has_many :groups, through: :user_groups
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /.+@.+\..+/ }
 end
