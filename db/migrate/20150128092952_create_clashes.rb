0f41f758da85d01be173672e3d19cdb404e9df98
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 09:38:26 -0500

    Builds clash migration

--- db/migrate/20150128092952_create_clashes.rb
+++ db/migrate/20150128092952_create_clashes.rb
@@ -1,4 +1,11 @@
 class CreateClashes < ActiveRecord::Migration
   def change
+    create_table :clashes do |t|
+      t.string :winner
+      t.string :runner_up
+      t.string :fool
+      t.belongs_to :game
+      t.timestamps
+    end
   end
 end
