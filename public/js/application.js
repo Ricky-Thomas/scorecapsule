19efa8ea156c8871a37504762d32e68a4e0298c5
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 14:20:11 -0500

    Ajax call to pop in Game creation form

--- public/js/application.js
+++ public/js/application.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
   $("#go_login").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" })
   $("#go_signup").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" })
 
+  var $add_game_button = $('#add_game_button');
+
+  $add_game_button.on('submit', function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+
+    $.ajax({
+      type: 'POST',
+      url: $target.attr('action'),
+      data: $target.serialize()
+    }).done(function(response) {
+      $('#group_games_ct').append(response);
+    })
+  })
 });
