87e9247f9d32e137ed67ae4adf0484bf920b43de
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 19:53:36 -0500

    Edits leader field in game table to be an integer

--- db/migrate/20150128092945_create_games.rb
+++ db/migrate/20150128092945_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :name
-      t.string :leader
+      t.integer :leader
       t.belongs_to :group
       t.timestamps
     end
