7ea69e2b3953024e7e49231147c97a8dee7ef5b1
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-28 19:09:40 -0500

    adds get_leader method to game class

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ActiveRecord::Base
   belongs_to :group
   has_many :clashes
+
+  def get_leader
+    winners = []
+    self.clashes.each {|clash| winners << clash.winner}
+    leader = winners.group_by {|i| i}.max_by {|k, v| v.count }[0]
+  end
 end
